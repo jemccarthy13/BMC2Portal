eb08026c6443fdbcd1c458b7c2e6f020b88259c5
jmccarthy <jmccarthy@shoc.us>
2020-12-09 19:06:55 -0600

    admin cleanup

--- parrotsour/src/changelog.tsx
+++ parrotsour/src/changelog.tsx
@@ -9,11 +9,18 @@ function ChangeLog():JSX.Element {
             --------------------------------------- <br/>
             Scheduled Features: <br/>
             <li> Verify anchoring P&quot;s in WALL, CHAMP TRAIL gps, CAP </li>
-            <li> Verify typescript conversion went well (i.e. no bugs in animation or answers)</li>
             <li> Finish opening/closing comm (particularly for range pics) </li>
             <li> CAP for more picture types </li>
             <li> Procedural simulation </li>
             --------------------------------------- <br/>
+            --------------------------------------- <br/> 
+            Version 3.1.0 - 09 Dec 2020 <br/>
+            --------------------------------------- <br/>
+            Features
+            <li>Finished conversion to typescript - also React Component library for varied use(s)</li>
+            <li>Fixed small issue in package comm (labeling/anchoring)</li>
+            <li>Implemented radar/arrow toggle (make it look like radar returns instead of arrows)</li>
+            <li>Migrated procedural implementation -- working on further development</li>
             --------------------------------------- <br/> 
             Version 3.0.5 - 21 Nov 2020 <br/>
             --------------------------------------- <br/>
