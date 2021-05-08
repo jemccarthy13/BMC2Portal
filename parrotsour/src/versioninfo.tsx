9fac5587e67de942a7d2d4073016d3900c656000
jmccarthy <jmccarthy@shoc.us>
2021-05-08 13:09:47 +0200

    styling updates and test

--- parrotsour/src/versioninfo.tsx
+++ parrotsour/src/versioninfo.tsx
@@ -1,23 +1,27 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement } from "react"
 
 /**
  * Component to display versioning information at the bottom of PS sites
  */
 export default class VersionInfo extends React.PureComponent {
+  vStyle = {
+    color: "lightblue",
+    paddingTop: "200px",
+    paddingBottom: "20px",
+    fontSize: 18,
+  }
 
-    vStyle = {
-        color:"lightblue"
-    }
-
-    render(): ReactElement {
-        return(
-            <div style={this.vStyle}>
-                 <br /><br /><br /><br /><br /><br /><br />
-                Developed by John McCarthy <br />
-                Version: 3.1.0 --
-                <a style={this.vStyle} href="/#/changelog.html"> Change Log </a> <br />
-                14 Dec 2020 <br />
-            </div>
-        )
-    }
+  render(): ReactElement {
+    return (
+      <div id="vInfo" style={this.vStyle}>
+        Developed by John McCarthy <br />
+        Version:&nbsp;
+        <a style={{ color: "#7978FD" }} href="/#/changelog.html">
+          3.1.0
+        </a>
+        <br />
+        14 Dec 2020 <br />
+      </div>
+    )
+  }
 }
