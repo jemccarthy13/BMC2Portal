039077c64cee750a3474b6c7cccfeb0f534503a3
jmccarthy <jmccarthy@shoc.us>
2021-05-09 16:27:33 +0200

    initial leading edge final fix

--- parrotsour/src/canvas/draw/intercept/leadingedge.ts
+++ parrotsour/src/canvas/draw/intercept/leadingedge.ts
@@ -1,6 +1,5 @@
 // Interfaces
 import {
-  BlueInThe,
   PictureAnswer,
   PictureDrawFunction,
   PictureCanvasProps,
@@ -9,9 +8,10 @@ import {
 } from "../../../canvas/canvastypes"
 
 // Functions
-import { drawLine, drawMeasurement } from "../drawutils"
+import { drawMeasurement } from "../drawutils"
 import { Point } from "../../../classes/point"
-import { PIXELS_TO_NM } from "../../../utils/psmath"
+import { getRestrictedStartPos } from "./pictureclamp"
+import { FORMAT } from "../../../classes/supportedformats"
 
 export const drawLeadEdge: PictureDrawFunction = (
   ctx: CanvasRenderingContext2D,
@@ -22,23 +22,29 @@ export const drawLeadEdge: PictureDrawFunction = (
   const isNS = FightAxis.isNS(props.orientation.orient)
 
   // // Draw the first picture (i.e. the leading edge)
-  // const pic1Bounds: Bounds = {
-  //   tall: { lowX: 0.2, hiX: 0.8, lowY: 0.3, hiY: 0.5 },
-  //   wide: { lowX: 0.55, hiX: 0.7, lowY: 0.2, hiY: 0.7 }
-  // }
-  //const pic1StartPos = getStartPos(ctx, props.orientation.orient, pic1Bounds, start)
-  //const answer1 = state.reDraw(ctx, true, pic1StartPos)
-  //const groups1 = answer1.groups;
+  const pic1StartPos = getRestrictedStartPos(
+    ctx,
+    state.blueAir,
+    props.orientation.orient,
+    props.dataStyle,
+    45,
+    100,
+    { start }
+  )
+  const answer1 = state.reDraw(ctx, true, pic1StartPos)
+  const groups1 = answer1.groups
 
-  //let furthestPic1Group = groups1[0]
-  //let furthestRange = 0
-  //groups1.forEach((grp) => {
-  //const grpRange = grp.getCenterOfMass().getBR(state.blueAir.getCenterOfMass()).range
-  //if (grpRange > furthestRange){
-  //furthestPic1Group = grp
-  // furthestRange = grpRange
-  //  }
-  //})
+  let furthestPic1Group = groups1[0]
+  let furthestRange = 0
+  groups1.forEach((grp) => {
+    const grpRange = grp
+      .getCenterOfMass(props.dataStyle)
+      .getBR(state.blueAir.getCenterOfMass(props.dataStyle)).range
+    if (grpRange > furthestRange) {
+      furthestPic1Group = grp
+      furthestRange = grpRange
+    }
+  })
 
   //
   // For a picture to be 'leading edge', the leading edge must be
@@ -48,62 +54,57 @@ export const drawLeadEdge: PictureDrawFunction = (
   // This logic builds the required separation and gets the
   // second picture starting point
   //
-  //const pic1TrailPos = furthestPic1Group.getCenterOfMass()
-  //const canvasSize = isNS ? ctx.canvas.height : ctx.canvas.width
 
-  //const limitLine = isNS ? pic1TrailPos.y : pic1TrailPos.x
+  // TODO -- LEAD EDGE -- this restricted start pos should be based on the
+  // first picture. but it should be directional (getRestrictedStartPos is not?)
+  // and should be rngBack (distStraightNM).. if lead edge is in front return (neg?)
+  // and clamp; if lead edge is behind, make sure it's at least 5 nm.
+  //
+  // What a pain.
+  const pic2StartPos = getRestrictedStartPos(
+    ctx,
+    furthestPic1Group,
+    props.orientation.orient,
+    props.dataStyle,
+    25,
+    40
+  )
+  const answer2 = state.reDraw(ctx, true, pic2StartPos)
+  const groups2 = answer2.groups
 
-  //const uBound = limitLine / canvasSize
-  //const lBound = (limitLine - (40*PIXELS_TO_NM)) / canvasSize
+  let closestPic2Group = groups2[0]
+  let closestRange = Number.MAX_VALUE
+  groups2.forEach((grp) => {
+    const grpRange = grp
+      .getCenterOfMass(props.dataStyle)
+      .getBR(state.blueAir.getCenterOfMass(props.dataStyle)).range
+    if (grpRange < closestRange) {
+      closestPic2Group = grp
+      closestRange = grpRange
+    }
+  })
 
-  // drawLine(ctx,
-  //   isNS ? 0 : limitLine - 5*PIXELS_TO_NM,
-  //   isNS ? limitLine-5*PIXELS_TO_NM : 0,
-  //   isNS ? ctx.canvas.width : limitLine -5*PIXELS_TO_NM,
-  //   isNS ? limitLine-5*PIXELS_TO_NM: ctx.canvas.height  )
+  const pic2Pos = closestPic2Group.getCenterOfMass(props.dataStyle)
+  const pic1Pos = furthestPic1Group.getCenterOfMass(props.dataStyle)
 
-  // const pic2Bounds: Bounds = {
-  //   tall: { lowX: 0.2, hiX: 0.8, lowY: lBound, hiY: uBound },
-  //   wide: { lowX: lBound, hiX: uBound, lowY: 0.2, hiY: 0.8 }
-  // }
+  const rngBack = pic1Pos.straightDistNM(pic2Pos, props.orientation.orient)
 
-  //const pic2StartPos = getStartPos(ctx, state.blueAir, props.orientation.orient, start )
+  const drawXTo = isNS ? pic1Pos.x : pic2Pos.x
+  const drawYTo = isNS ? pic2Pos.y : pic1Pos.y
+  drawMeasurement(ctx, pic1Pos.x, pic1Pos.y, drawXTo, drawYTo, rngBack, true)
 
-  //const answer2 = state.reDraw( ctx, true, pic2StartPos )
-  //const groups2 = answer2.groups
-
-  //let closestPic2Group = groups1[0]
-  //let closestRange = Number.MAX_VALUE
-  //groups2.forEach((grp) => {
-  //  const grpRange = grp.getCenterOfMass().getBR(state.blueAir.getCenterOfMass()).range
-  //  if (grpRange < closestRange){
-  //    closestPic2Group = grp
-  //    closestRange = grpRange
-  //  }
-  //})
-
-  //const pic2Pos = closestPic2Group.getCenterOfMass()
-  //const pic1Pos = furthestPic1Group.getCenterOfMass()
-
-  //const rngBack = pic1Pos.straightDistNM(pic2Pos, props.orientation.orient)
-
-  //const drawXTo = (isNS ? pic1Pos.x : pic2Pos.x)
-  //const drawYTo = (isNS ? pic2Pos.y : pic1Pos.y)
-  //drawMeasurement(ctx, pic1Pos.x, pic1Pos.y, drawXTo, drawYTo, rngBack, true)
-
-  //const finalAnswer = {
-  //  pic:
-  //    (( groups1.length + groups2.length ) + "GROUPS, LEADING EDGE " +
-  //    answer1.pic + " FOLLOW ON " +
-  //    (props.format === "ipe" ? " GROUPS " : "")+
-  //    rngBack +
-  //   (props.format === "ipe" ? " MILES " : "")),
-  // groups: groups1.concat(groups2)
-  //}
-  //console.log(finalAnswer)
-  return {
-    pic: "",
-    groups: [],
+  const finalAnswer = {
+    pic:
+      groups1.length +
+      groups2.length +
+      "GROUPS, LEADING EDGE " +
+      answer1.pic +
+      " FOLLOW ON " +
+      (props.format === FORMAT.IPE ? " GROUPS " : "") +
+      rngBack +
+      (props.format === FORMAT.IPE ? " MILES " : ""),
+    groups: groups1.concat(groups2),
   }
-  //return finalAnswer;
+
+  return finalAnswer
 }
