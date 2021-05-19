6950c14e0c1aab046ea342c6fbc3da6544bce607
jmccarthy <jmccarthy@shoc.us>
2021-05-19 11:00:07 +0200

    fixing console error for window func not implemented

--- parrotsour/src/changelog/changelog.int.test.tsx
+++ parrotsour/src/changelog/changelog.int.test.tsx
@@ -7,6 +7,10 @@ import ChangeLog from "./changelog"
  * I really don't care what the changelog looks like, as long as it renders something.
  */
 describe("ChangeLog", () => {
+  beforeAll(() => {
+    window.scrollTo = jest.fn()
+  })
+
   it("renders_with_children", () => {
     const wrapper = mount(<ChangeLog />)
     expect(wrapper.find(Accordion)).toBeDefined()
